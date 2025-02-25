91ec3762a15b0dfc2d68f51d5b8bf1b41f27a326
Sanidhya Mishra <sanidhyamishra1808@gmail.com>
2025-02-25 22:24:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Global Variables
 var DIRECTION = {
     IDLE: 0,
     UP: 1,
@@ -101,31 +100,30 @@ var Game = {
             100
         );
  
-        // Change the canvas color;
         this.context.fillStyle = '#ffffff';
  
-        // Draw the 'press any key to begin' text
         this.context.fillText('Press any key to begin',
             this.canvas.width / 2,
             this.canvas.height / 2 + 15
         );
     },
- 
-    // Update all objects (move the player, ai, ball, increment the score, etc.)
+    triggerGlowEffect: function () {
+        this.canvas.classList.add("glow-effect");
+        setTimeout(() => {
+            this.canvas.classList.remove("glow-effect");
+        }, 300);
+    },
+    
     update: function () {
         if (!this.over) {
-            // If the ball collides with the bound limits - correct the x and y coords.
             if (this.ball.x <= 0) Pong._resetTurn.call(this, this.ai, this.player);
             if (this.ball.x >= this.canvas.width - this.ball.width) Pong._resetTurn.call(this, this.player, this.ai);
             if (this.ball.y <= 0) this.ball.moveY = DIRECTION.DOWN;
             if (this.ball.y >= this.canvas.height - this.ball.height) this.ball.moveY = DIRECTION.UP;
  
-            // Move player if they player.move value was updated by a keyboard event
             if (this.player.move === DIRECTION.UP) this.player.y -= this.player.speed;
             else if (this.player.move === DIRECTION.DOWN) this.player.y += this.player.speed;
  
-            // On new serve (start of each turn) move the ball to the correct side
-            // and randomize the direction to add some challenge.
             if (Pong._turnDelayIsOver.call(this) && this.turn) {
                 this.ball.moveX = this.turn === this.player ? DIRECTION.LEFT : DIRECTION.RIGHT;
                 this.ball.moveY = [DIRECTION.UP, DIRECTION.DOWN][Math.round(Math.random())];
@@ -133,17 +131,14 @@ var Game = {
                 this.turn = null;
             }
  
-            // If the player collides with the bound limits, update the x and y coords.
             if (this.player.y <= 0) this.player.y = 0;
             else if (this.player.y >= (this.canvas.height - this.player.height)) this.player.y = (this.canvas.height - this.player.height);
  
-            // Move ball in intended direction based on moveY and moveX values
             if (this.ball.moveY === DIRECTION.UP) this.ball.y -= (this.ball.speed / 1.5);
             else if (this.ball.moveY === DIRECTION.DOWN) this.ball.y += (this.ball.speed / 1.5);
             if (this.ball.moveX === DIRECTION.LEFT) this.ball.x -= this.ball.speed;
             else if (this.ball.moveX === DIRECTION.RIGHT) this.ball.x += this.ball.speed;
  
-            // Handle ai (AI) UP and DOWN movement
             if (this.ai.y > this.ball.y - (this.ai.height / 2)) {
                 if (this.ball.moveX === DIRECTION.RIGHT) this.ai.y -= this.ai.speed / 1.5;
                 else this.ai.y -= this.ai.speed / 4;
@@ -153,30 +148,26 @@ var Game = {
                 else this.ai.y += this.ai.speed / 4;
             }
  
-            // Handle ai (AI) wall collision
             if (this.ai.y >= this.canvas.height - this.ai.height) this.ai.y = this.canvas.height - this.ai.height;
             else if (this.ai.y <= 0) this.ai.y = 0;
  
-            // Handle Player-Ball collisions
             if (this.ball.x - this.ball.width <= this.player.x && this.ball.x >= this.player.x - this.player.width) {
                 if (this.ball.y <= this.player.y + this.player.height && this.ball.y + this.ball.height >= this.player.y) {
                     this.ball.x = (this.player.x + this.ball.width);
                     this.ball.moveX = DIRECTION.RIGHT;
- 
+                    this.triggerGlowEffect();
                 }
             }
- 
-            // Handle ai-ball collision
+            
             if (this.ball.x - this.ball.width <= this.ai.x && this.ball.x >= this.ai.x - this.ai.width) {
                 if (this.ball.y <= this.ai.y + this.ai.height && this.ball.y + this.ball.height >= this.ai.y) {
                     this.ball.x = (this.ai.x - this.ball.width);
                     this.ball.moveX = DIRECTION.LEFT;
- 
+                    this.triggerGlowEffect();
                 }
             }
         }
  
-        
         if (this.player.score === rounds[this.round]) {
             
             if (!rounds[this.round + 1]) {
@@ -285,7 +276,6 @@ var Game = {
         Pong.update();
         Pong.draw();
  
-        // If the game is not over, draw the next frame.
         if (!Pong.over) requestAnimationFrame(Pong.loop);
     },
  
